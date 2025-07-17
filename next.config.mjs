68b1dc6f206862702d011965cff651afb2fd574e
Kirubakarankavirajan <kirubakaran.kavirajan@highway905.com>
2025-07-17 13:00:08 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   env: {
-    HOST_API_KEY: process.env.HOST_API_KEY || "http://localhost:7000/",
-    HOST_API_FE: process.env.HOST_API_FE || "http://localhost:3000/",
-    ENCRYPT_PUBLIC_KEY: process.env.ENCRYPT_PUBLIC_KEY || "-----BEGIN PUBLIC KEY-----\\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAzKNt9jK8YmZ8iU6eydM4\\nYzQn0SdlFkWnj2j0QKdEKrIYCJgIwOYXRsH3rvxYpKMgJ8wS4BaraAPDFNGVBQXM\\nPQXEGLWZVyqQTyhCrxn4AJe6XVV2is/QEKyIG7bMNA/CSCIRGVvkRd+n9MmTXEYV\\nTMGzIeQj7Jj8YR+kQOwM+GTxBFn/pLQPgLwPgHHNwNqZQCnzYUBGY8N2y5/BUPzI\\nTEHSz/bQcW0RRdvYHfJ+QJNM7QcTVdTgZt7ewR7J+UIE5XxJWQQ1XKNI/BmI2nVF\\n5wUEjgLrDQnCJQ+87V0YeKjRMbfwMiPP+1vQZ5KOtKz7bkYAZUVFmgKc9MZGIXXy\\nrwIDAQAB\\n-----END PUBLIC KEY-----"
+    HOST_API_KEY: process.env.HOST_API_KEY || "http://52.249.214.158/",
+    HOST_API_FE: process.env.HOST_API_FE || "http://4.157.146.207/",
+    ENCRYPT_PUBLIC_KEY: process.env.ENCRYPT_PUBLIC_KEY || "-----BEGIN PUBLIC KEY-----\\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCKNOjgcbVefxYyNP05VE74Azxt\\nywzM6qsQuAQc/Y75MUm4q03tO6BCwAH+dSJXkxloA9yol0ve44i9bVRu45uyRlTX\\ny9AenA6Vg54ECZ6s/as6wbqvTICTBAk3dUU3bOG3lGRkJYzbsyyWjTl7AiSI3Z4B\\nqyimdBzSBkVqLTCa2QIDAQAB\\n-----END PUBLIC KEY-----"
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
