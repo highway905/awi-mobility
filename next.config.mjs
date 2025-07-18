d333ac07cca6f3c24705a2dfdcaeefee050b13d8
Selvakumar Panchatcharam <selvakumar.panchatcharam@highway905.com>
2025-07-18 15:02:56 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
   env: {
-    HOST_API_KEY: process.env.HOST_API_KEY || "http://52.249.214.158/",
-    HOST_API_FE: process.env.HOST_API_FE || "http://4.157.146.207/",
+    HOST_API_KEY: process.env.HOST_API_KEY || "http://api-qa-gateway-service:7000/",
+    HOST_API_FE: process.env.HOST_API_FE || "http://h905awi-qa-mobility-service:3000/",
     ENCRYPT_PUBLIC_KEY: process.env.ENCRYPT_PUBLIC_KEY || "-----BEGIN PUBLIC KEY-----\\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCKNOjgcbVefxYyNP05VE74Azxt\\nywzM6qsQuAQc/Y75MUm4q03tO6BCwAH+dSJXkxloA9yol0ve44i9bVRu45uyRlTX\\ny9AenA6Vg54ECZ6s/as6wbqvTICTBAk3dUU3bOG3lGRkJYzbsyyWjTl7AiSI3Z4B\\nqyimdBzSBkVqLTCa2QIDAQAB\\n-----END PUBLIC KEY-----"
   },
   webpack: (config, { isServer }) => {
